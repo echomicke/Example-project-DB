1cd72e3689fbad4051eb4fa0c08ec806111d9be9
echomicke <michael.kamara@chasacademy.se>
2023-02-01 22:56:34 +0100

    Initial version of the library

---
+++ DBS_version1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='mypythonlib',
+    packages=find_packages(include=['DBSpythonlib']),
+    version='0.0.1',
+    description='Initial DBS library',
+    author='Code Blasters',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests'
+)
